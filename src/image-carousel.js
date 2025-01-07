bea37ac362c03da2127d059e40b5284682258b1d
Javier Quiroga <contact@javierquiroga.com>
2025-01-07 23:02:53 +0000

    Add dot indicators

--- src/image-carousel.js
+++ src/image-carousel.js
@@ -1,8 +1,11 @@
-function imgCarousel(targetId, timeoutDelay) {
+function imageCarousel(targetId, timeoutDelay) {
   const carouselContainer = document.getElementById(`${targetId}`);
   const nextArrow = carouselContainer.querySelector('.arrow-r');
   const prevArrow = carouselContainer.querySelector('.arrow-l');
   const imgArr = carouselContainer.querySelectorAll('img');
+  const dotNavWrapper = carouselContainer.querySelector(
+    '.dot-navigation-wrapper'
+  );
   const carouselLength = imgArr.length - 1;
   let currentImg = 0; // Counter to keep track of the image currently being shown
   let autoNextImg; // Variable to store the setTimeout
@@ -26,6 +29,7 @@ function imgCarousel(targetId, timeoutDelay) {
     }
     return currentImg;
   }
+
   function getPrevImg() {
     if (currentImg > 0) {
       currentImg--;
@@ -38,6 +42,7 @@ function imgCarousel(targetId, timeoutDelay) {
   // Scrolls to selected image
   function scrollImg(i) {
     imgArr[i].scrollIntoView();
+    selectDot(i);
     if (timeoutDelay) restartTimeout(); // Restarts the timer if there is one
   }
 
@@ -50,8 +55,23 @@ function imgCarousel(targetId, timeoutDelay) {
     scrollImg(getPrevImg());
   }
 
+  // Adds dot navigation
+  imgArr.forEach(() => {
+    const singleDot = document.createElement('div');
+    singleDot.classList.add('nav-dot');
+    dotNavWrapper.appendChild(singleDot);
+  });
+  const dotArr = dotNavWrapper.querySelectorAll('.nav-dot');
+
+  // Adds class to the corresponding dot
+  function selectDot(i) {
+    dotArr.forEach((dot) => dot.classList.remove('selected-dot'));
+    dotArr[i].classList.add('selected-dot');
+  }
+
   nextArrow.addEventListener('click', scrollNextImg);
   prevArrow.addEventListener('click', scrollPrevImg);
+  selectDot(0);
 }
 
-export default imgCarousel;
+export default imageCarousel;
